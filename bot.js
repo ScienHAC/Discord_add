0958010864aae62e2cb61dc266baa4118adcd1ed
Piyush Sharma <90610178+ScienHAC@users.noreply.github.com>
2024-09-26 21:40:04 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,93 +1,3 @@
-/*const { Client, GatewayIntentBits } = require('discord.js');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const TOKEN = process.env.TOKEN;
-const CLIENT_ID = process.env.CLIENT_ID; // Client ID from the Developer Portal
-
-// Create a new client instance
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildMessageReactions,
-  ],
-});
-
-// Register the slash command globally (can be used across any guild where the bot is invited)
-const commands = [
-  {
-    name: 'add-user',
-    description: 'Add a user to private and public channels',
-    options: [
-      {
-        type: 6, // USER type (user ID)
-        name: 'usr',
-        description: 'The user to add',
-        required: true,
-      },
-    ],
-  },
-];
-
-const rest = new REST({ version: '9' }).setToken(TOKEN);
-
-// Register the slash command globally (across all servers where the bot is added)
-(async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
-    await rest.put(
-      Routes.applicationCommands(CLIENT_ID),
-      { body: commands },
-    );
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
-})();
-
-// When the bot is ready
-client.once('ready', async () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-// Command handling
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  const { commandName } = interaction;
-
-  if (commandName === 'add-user') {
-    const user = interaction.options.getUser('usr');
-    const guild = interaction.guild;
-
-    // Fetch all channels in the guild
-    const channels = await guild.channels.fetch();
-
-    try {
-      // Add the user to all available text channels
-      for (const [channelId, channel] of channels) {
-        if (channel.isTextBased()) {
-          // Ensure the channel is manageable
-          if (channel.permissionsFor(guild.me).has('MANAGE_CHANNELS')) {
-            await channel.permissionOverwrites.edit(user, {
-              VIEW_CHANNEL: true,
-            });
-            console.log(`Added ${user.tag} to channel ${channel.name}`);
-          } else {
-            console.log(`Bot lacks permission to edit channel: ${channel.name}`);
-          }
-        }
-      }
-
-      // Respond to the interaction that the user was added
-      await interaction.reply(`User ${user.tag} added to all text channels.`);
-    } catch (error) {
-      console.error('Error adding user to channels:', error);
-      await interaction.reply('There was an error while trying to add the user.');
-    }
-  }
-});*/
 const { Client, GatewayIntentBits } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
@@ -213,7 +123,11 @@ client.on('interactionCreate', async (interaction) => {
         }
       }
 
-      await interaction.followUp(`User ${user.tag} (ID: ${user.id}) has been added to ${successCount} channels.\n${failureCount > 0 ? `${failureCount} failures:\n${failureReasons.join('\n')}` : ''}`);
+      // If too many failures, limit the message size
+      const failureMessage = failureReasons.slice(0, 10).join('\n'); // Show only the first 10 failures
+      const remainingFailures = failureCount > 10 ? `\n...and ${failureCount - 10} more failures.` : '';
+
+      await interaction.followUp(`User ${user.tag} (ID: ${user.id}) has been added to ${successCount} channels.\n${failureCount > 0 ? `${failureCount} failures (showing first 10):\n${failureMessage}${remainingFailures}` : ''}`);
     } catch (error) {
       console.error('Error adding user to all channels:', error);
       await interaction.followUp('There was an error while trying to add the user to all channels.');
@@ -239,5 +153,3 @@ client.on('interactionCreate', async (interaction) => {
 
 // Login to Discord with your bot's token
 client.login(TOKEN);
-
-
